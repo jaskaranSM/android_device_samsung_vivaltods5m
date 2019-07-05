cad9e04dd6c77a34707c46e09993da1ab21073df
jaskaranSM <37726998+jaskaranSM@users.noreply.github.com>
2019-07-05 13:42:07 +0530

    Update and rename lineage_vivaltods5m.mk to lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -1,19 +1,16 @@
-# Release name
-PRODUCT_RELEASE_NAME := SM-G313HU
-
-# Inherit APNs list
-$(call inherit-product, vendor/lineage/config/gsm.mk)
-
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/lineage/config/common.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/vivaltods5m/device_vivaltods5m.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := SM-G313HU
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := vivaltods5m
 PRODUCT_NAME := lineage_vivaltods5m
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
-PRODUCT_MODEL := lineage on Vivalto
+PRODUCT_MODEL := SM-G313HU
 PRODUCT_CHARACTERISTICS := phone
